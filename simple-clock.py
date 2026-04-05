7d818838997757cc1551d1e6866a8565b83732db
james <J.Joseph.Taylor@obyliskconsulting.com>
2026-04-05 19:40:55 -0400

    Fonts corrected and button frame added.

--- simple-clock.py
+++ simple-clock.py
@@ -11,9 +11,12 @@ class SimpleClock(tk.Tk):
         self.resizable(False, False)
         self.configure(bg="black")
 
+        self._use_24hr = False
+        self._usegmt = False
+
         self.time_label = tk.Label(
             self,
-            font=("Sans-serif", 59, "normal"),
+            font=("Sans-serif", 40, "normal"),
             bg="black",
             fg="white",
             pady=10,
@@ -22,7 +25,7 @@ class SimpleClock(tk.Tk):
 
         self.day_label = tk.Label(
             self,
-            font=("Ink Free", 34, "bold"),
+            font=("Ink Free", 28, "bold"),
             bg="black",
             fg="white",
         )
@@ -30,9 +33,16 @@ class SimpleClock(tk.Tk):
 
         self.date_label = tk.Label(
             self,
-            font=("Ink Free", 30, "bold"),
+            font=("Ink Free", 18, "bold"),
+            bg="black",
+            fg="white",
+            pady=6,
         )
         self.date_label.pack()
+
+        button_frame = tk.Frame(self, bg="black", pady=8)
+        button_frame.pack()
+
 
         self._update()
 
