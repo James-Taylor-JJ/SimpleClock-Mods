e5986485f690f848d44ebf477112b7df14a1ef88
james <J.Joseph.Taylor@obyliskconsulting.com>
2026-04-05 20:02:59 -0400

    Update function debugged

--- simple-clock.py
+++ simple-clock.py
@@ -1,10 +1,12 @@
 import tkinter as tk
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 class SimpleClock(tk.Tk):
 
     def __init__(self):
+        self._use_24hr = False                                           
+        self._use_gmt = False
         super().__init__()
         self.title("Digital Clock")
         self.geometry("350x280")
@@ -59,16 +61,18 @@ class SimpleClock(tk.Tk):
     
     def _toggle_format(self):                                           
         self._use_24hr = not self._use_24hr                             
-        self._fmt_btn.config(text="12 hr" if self._use_24hr else "24 hr")  
+        self._fmt_btn.config(text="12 hr" if self._use_24hr else "24 hr")
+        self.update()  
  
     def _toggle_timezone(self):                                         
         self._use_gmt = not self._use_gmt                               
         self._tz_btn.config(text="Local" if self._use_gmt else "GMT")  
-       
+        self.update()  
 
     def _update(self):
         now = datetime.now(timezone.utc) if self._use_gmt else datetime.now()
-        self.time_label.config(text=now.strftime("%I:%M:%S %p"))
+        time_fmt = "%H:%M:%S" if self._use_24hr else "%I:%M:%S %p"
+        self.time_label.config(text=now.strftime(time_fmt))
         self.day_label.config(text=now.strftime("%A"))
         self.date_label.config(text=now.strftime("%d %B, %Y"))
         self.after(1000, self._update)
